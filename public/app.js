86e2ed16a6c5fed56d8a81939112a23d0c4bd3d2
SilvesterSpath <spath.sz@gmail.com>
2022-04-21 07:45:25 +0200

    edit checks

--- public/app.js
+++ public/app.js
@@ -293,7 +293,11 @@ app.formResponseProcessor = (formId, requestPayload, responsePayload) => {
   }
 
   // If form saved successfully and they have success messages, show them
-  const formsWithSuccessMessages = ['accountEdit1', 'accountEdit2'];
+  const formsWithSuccessMessages = [
+    'accountEdit1',
+    'accountEdit2',
+    'checksEdit1',
+  ];
   if (formsWithSuccessMessages.indexOf(formId) > -1) {
     document.querySelector('#' + formId + ' .formSuccess').style.display =
       'block';
@@ -422,6 +426,11 @@ app.loadDataOnPage = () => {
   // Logic for dashboard page
   if (primaryClass == 'checksList') {
     app.loadChecksListPage();
+  }
+
+  // Logic for check details page
+  if (primaryClass == 'checksEdit') {
+    app.loadChecksEditPage();
   }
 };
 
@@ -560,6 +569,67 @@ app.loadChecksListPage = () => {
     );
   } else {
     app.logUserOut();
+  }
+};
+
+// Load the checks edit page specifically
+app.loadChecksEditPage = () => {
+  // Get the check id from the query string, if none is then redirect back to dashboard
+  const id =
+    typeof window.location.href.split('=')[1] == 'string' &&
+    window.location.href.split('=')[1].length > 0
+      ? window.location.href.split('=')[1]
+      : false;
+  if (id) {
+    // Fetch the check data
+    const queryStringObject = {
+      id: id,
+    };
+    app.client.request(
+      undefined,
+      'api/checks',
+      'GET',
+      queryStringObject,
+      undefined,
+      (statusCode, responsePayload) => {
+        if (statusCode == 200) {
+          // Put the id into the 'hidden id input' field in both forms
+          const hiddenIdInputs = document.querySelectorAll(
+            'input.hiddenIdInput'
+          );
+          hiddenIdInputs.forEach((i) => {
+            i.value = responsePayload.id;
+          });
+
+          // Put the data into the top form as values where needed
+          document.querySelector('#checksEdit1 .displayIdInput').value =
+            responsePayload.id;
+          document.querySelector('#checksEdit1 .displayStateInput').value =
+            responsePayload.state;
+          document.querySelector('#checksEdit1 .protocolInput').value =
+            responsePayload.protocol;
+          document.querySelector('#checksEdit1 .urlInput').value =
+            responsePayload.url;
+          document.querySelector('#checksEdit1 .methodInput').value =
+            responsePayload.method;
+          document.querySelector('#checksEdit1 .timeoutInput').value =
+            responsePayload.timeoutSeconds;
+          const succesCodeCheckboxes = document.querySelectorAll(
+            '#checksEdit1 input.successCodesInput'
+          );
+          succesCodeCheckboxes.forEach((i) => {
+            if (responsePayload.successCodes.indexOf(parseInt(i.value)) > -1) {
+              i.checked = true;
+            }
+          });
+        } else {
+          // If the request comes back as something other than 200, redirect back to dashboard
+          window.location = 'checks/all';
+        }
+      }
+    );
+  } else {
+    window.location = 'checks/all';
   }
 };
 
