cd408fce5969f13b1e06d2c509b0a1078e466c8b
SilvesterSpath <spath.sz@gmail.com>
2022-05-03 07:01:44 +0200

    strict mode

---
+++ index-strict.js
@@ -0,0 +1,38 @@
+/*
+ * Primary file for the API
+ *
+ */
+
+// Dependencies
+require('dotenv').config({ path: './.env' });
+const server = require('./lib/server');
+const workers = require('./lib/workers');
+const cli = require('./lib/cli');
+
+// Declare the app
+const app = {};
+
+// Declare a global (that strict mode should catch)
+foo = 'bar';
+
+// Init function
+app.init = () => {
+  // Start the server
+  server.init();
+
+  // Start the workers
+  workers.init();
+
+  // Start the CLI, but make sure its starts last
+  setTimeout(() => {
+    cli.init();
+  }, 50);
+};
+
+// Execute that function
+app.init();
+
+console.log('process.env.TEST:', process.env.TEST);
+
+// Export the app
+module.exports = app;
