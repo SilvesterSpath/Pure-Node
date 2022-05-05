00241a67c733521400ddbfa82d9503e100f2d9a8
SilvesterSpath <spath.sz@gmail.com>
2022-05-05 07:55:39 +0200

    api tests init

---
+++ test/api.js
@@ -0,0 +1,19 @@
+/*
+ * API tests
+ */
+
+// Dependencies
+const api = require('../index');
+const assert = require('assert');
+const http = require('http');
+const config = require('../lib/config');
+
+// Holder for the tests
+const api = {};
+
+// Helpers
+const helpers = {};
+helpers.makeGetRequest = (path, callback) => {};
+
+// Export the module
+module.exports = api;
